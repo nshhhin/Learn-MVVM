cc921d9df636dcf7dfc18dd830261feda70578a0
nshhhin <shinn070110@gmail.com>
2019-10-04 23:34:18 +0900

    :sparkles: サンプルコードを入力

--- Learn-MVVM-Swift/ViewController.swift
+++ Learn-MVVM-Swift/ViewController.swift
@@ -1,11 +1,23 @@
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.textField.rx.text
+        .bind(to: self.label.rx.text)
+        .disposed(by: self.disposeBag)  // 不要になったら破棄
     }
+    
 
+    
 }
 
